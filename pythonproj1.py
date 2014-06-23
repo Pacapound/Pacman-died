86a2fdcd947fddf233fa042faf57fb8180c749b5
Pacapound <lukeluo2002@gmail.com>
2014-06-23 15:07:43 -0400

    UPDATE

--- pythonproj1.py
+++ pythonproj1.py
@@ -1,4 +1,3 @@
-import Tkinter
 from Tkinter import *
 import random
 import time
@@ -88,6 +87,28 @@ class Paddle:
 		self.x = -2
 	def turn_right(self, evt):
 		self.x = 2
+
+class Paddle2:
+	def __init__(self, canvas, color):
+		self.canvas = canvas
+		self.id = canvas.create_rectangle(0, 0, 100, 10, fill = color)
+		self.canvas.move(self.id, 200, 300)
+		self.x = 0
+		self.canvas.width = self.canvas.winfo_width
+		self.canvas.bind_all('<KeyPress-a>', self.turn_left)
+		self.canvas.bind_all('<KeyPress-d>', self.turn_right)
+	def draw(self):
+		self.canvas.move(self.id, self.x, 0)
+		pos = self.canvas.coords(self.id)
+		if pos[0] <= 0:
+			self.x = 0
+		elif pos[2] >= self.canvas.width:
+			self.x = 0
+	def turn_left(self, evt):
+		self.x = -2
+	def turn_right(self, evt):
+		self.x = 2
+	
 tk = Tk()
 tk.title("Pinballz")
 tk.resizable(0, 0)
@@ -97,16 +118,18 @@ canvas.pack()
 tk.update()
 
 paddle = Paddle(canvas, 'blue')
+paddle2 = Paddle2(canvas, 'yellow')
 ball = Ball(canvas, paddle, 'red', [-3, -2, -1, 0, 1, 2, 3])
-ball2 = Ball(canvas, paddle, 'green', [-4, -5, -6, 7, 4, 5, 6])
+ball2 = Ball(canvas, paddle2, 'green', [-4, -5, -6, 7, 4, 5, 6])
 
 
 
 while 1:
-	if ball.hit_bottom == False:
+	if ball2.hit_bottom == False:
 		ball.draw()
 		ball2.draw()
 		paddle.draw()
+		paddle2.draw()
 	tk.update_idletasks()
 	tk.update()
 	time.sleep(0.01)
